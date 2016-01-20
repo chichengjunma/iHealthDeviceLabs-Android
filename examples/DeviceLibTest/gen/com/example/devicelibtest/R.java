c3e6cfcf671fbe8034d1fba5ba9956f394cd7292
songby <806936973@qq.com>
2016-01-20 18:04:50 +0800

    modify jar

--- examples/DeviceLibTest/gen/com/example/devicelibtest/R.java
+++ examples/DeviceLibTest/gen/com/example/devicelibtest/R.java
@@ -17,12 +17,6 @@ public final class R {
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
- Default screen margins, per the Android Design guidelines. 
-
-         Example customization of dimensions originally defined in res/values/dimens.xml
-         (such as screen margins) for screens with more than 820dp of available width. This
-         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
-    
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -138,10 +132,9 @@ public final class R {
         public static final int am4=0x7f070001;
         public static final int bp3_l=0x7f070002;
         public static final int bp3_m=0x7f070003;
-        public static final int main=0x7f070004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050005;
         public static final int app_name=0x7f050000;
         public static final int bluetooth_devicelist_dialog_password=0x7f050012;
         public static final int bluetooth_devicelist_dialog_ssid=0x7f050010;
@@ -152,16 +145,16 @@ public final class R {
         public static final int ok=0x7f050014;
         public static final int title_activity_abi=0x7f050016;
         public static final int title_activity_am3=0x7f050017;
-        public static final int title_activity_am3_s=0x7f050003;
+        public static final int title_activity_am3_s=0x7f050002;
         public static final int title_activity_am3s=0x7f05001a;
         public static final int title_activity_am4=0x7f05001b;
         public static final int title_activity_bg1=0x7f05000d;
         public static final int title_activity_bg5=0x7f05000e;
         public static final int title_activity_bp3_l=0x7f050006;
         public static final int title_activity_bp3_m=0x7f050007;
-        public static final int title_activity_bp5=0x7f050004;
+        public static final int title_activity_bp5=0x7f050003;
         public static final int title_activity_bp550_bt=0x7f050019;
-        public static final int title_activity_bp7=0x7f050005;
+        public static final int title_activity_bp7=0x7f050004;
         public static final int title_activity_bp7s=0x7f050018;
         public static final int title_activity_bp926=0x7f05001c;
         public static final int title_activity_hs3=0x7f050009;
@@ -192,31 +185,9 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
